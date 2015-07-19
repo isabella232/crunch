6e0b4e3d953c99a9c8121a83d2b7658511b95087
Gabriel Reid <greid@apache.org>
2015-07-19 16:41:39 +0200

    CRUNCH-546 Remove calls to CellUtil.cloneXXX
    Remove (relatively expensive) calls to CellUtil.cloneXXX where
    possible, instead using alternate versions of Bytes.compareTo
    and Bytes.equals.

--- crunch-hbase/src/main/java/org/apache/crunch/io/hbase/HFileInputFormat.java
+++ crunch-hbase/src/main/java/org/apache/crunch/io/hbase/HFileInputFormat.java
@@ -148,8 +148,13 @@ public class HFileInputFormat extends FileInputFormat<NullWritable, KeyValue> {
         return false;
       }
       value = KeyValue.cloneAndAddTags(scanner.getKeyValue(), ImmutableList.<Tag>of());
-      if (stopRow != null && Bytes.compareTo(CellUtil.cloneRow(value), stopRow) >= 0) {
-        LOG.info("Reached stop row " + Bytes.toStringBinary(stopRow));
+      if (stopRow != null &&
+          Bytes.compareTo(
+              value.getRowArray(), value.getRowOffset(), value.getRowLength(),
+              stopRow, 0, stopRow.length) >= 0) {
+        if(LOG.isInfoEnabled()) {
+          LOG.info("Reached stop row " + Bytes.toStringBinary(stopRow));
+        }
         reachedStopRow = true;
         value = null;
         return false;
