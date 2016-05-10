339d3b129f796faf77d875147b161a0bcb3c54fd
Gabriel Reid <greid@apache.org>
2016-05-10 11:02:11 +0200

    CRUNCH-608 Write Bloom filters in HFiles
    Use a correctly-configured StoreFile.Writer (instead of HFile.Writer)
    for writing HFiles so that Bloom filter data is also included in
    the written HFiles.
    
    (cherry picked from commit c09c4ee2de992b50c15d2cb91a3e6e22c88fb0b1)

--- crunch-hbase/src/main/java/org/apache/crunch/io/hbase/HFileOutputFormatForCrunch.java
+++ crunch-hbase/src/main/java/org/apache/crunch/io/hbase/HFileOutputFormatForCrunch.java
@@ -22,8 +22,6 @@ package org.apache.crunch.io.hbase;
 import com.google.common.collect.ImmutableList;
 import org.apache.commons.codec.DecoderException;
 import org.apache.commons.codec.binary.Hex;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
@@ -32,10 +30,10 @@ import org.apache.hadoop.hbase.HColumnDescriptor;
 import org.apache.hadoop.hbase.HConstants;
 import org.apache.hadoop.hbase.KeyValue;
 import org.apache.hadoop.hbase.Tag;
+import org.apache.hadoop.hbase.io.hfile.CacheConfig;
 import org.apache.hadoop.hbase.io.hfile.HFile;
 import org.apache.hadoop.hbase.io.hfile.HFileContext;
-import org.apache.hadoop.hbase.io.hfile.HFileDataBlockEncoderImpl;
-import org.apache.hadoop.hbase.regionserver.HStore;
+import org.apache.hadoop.hbase.regionserver.BloomType;
 import org.apache.hadoop.hbase.regionserver.StoreFile;
 import org.apache.hadoop.hbase.regionserver.TimeRangeTracker;
 import org.apache.hadoop.hbase.util.Bytes;
@@ -43,6 +41,8 @@ import org.apache.hadoop.io.WritableUtils;
 import org.apache.hadoop.mapreduce.RecordWriter;
 import org.apache.hadoop.mapreduce.TaskAttemptContext;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.ByteArrayInputStream;
 import java.io.DataInputStream;
@@ -62,7 +62,7 @@ public class HFileOutputFormatForCrunch extends FileOutputFormat<Object, Cell> {
 
   public static final String HCOLUMN_DESCRIPTOR_KEY = "hbase.hfileoutputformat.column.descriptor";
   private static final String COMPACTION_EXCLUDE_CONF_KEY = "hbase.mapreduce.hfileoutputformat.compaction.exclude";
-  private static final Log LOG = LogFactory.getLog(HFileOutputFormatForCrunch.class);
+  private static final Logger LOG = LoggerFactory.getLogger(HFileOutputFormatForCrunch.class);
 
   private final byte [] now = Bytes.toBytes(System.currentTimeMillis());
   private final TimeRangeTracker trt = new TimeRangeTracker();
@@ -89,13 +89,16 @@ public class HFileOutputFormatForCrunch extends FileOutputFormat<Object, Cell> {
     }
     HColumnDescriptor hcol = new HColumnDescriptor();
     hcol.readFields(new DataInputStream(new ByteArrayInputStream(hcolBytes)));
-    LOG.info("Output path: " + outputPath);
-    LOG.info("HColumnDescriptor: " + hcol.toString());
-    final HFile.Writer writer = HFile.getWriterFactoryNoCache(conf)
-        .withPath(fs, outputPath)
+    LOG.info("Output path: {}", outputPath);
+    LOG.info("HColumnDescriptor: {}", hcol.toString());
+    Configuration noCacheConf = new Configuration(conf);
+    noCacheConf.setFloat(HConstants.HFILE_BLOCK_CACHE_SIZE_KEY, 0.0f);
+    final StoreFile.Writer writer = new StoreFile.WriterBuilder(conf, new CacheConfig(noCacheConf), fs)
         .withComparator(KeyValue.COMPARATOR)
         .withFileContext(getContext(hcol))
-        .create();
+        .withFilePath(outputPath)
+        .withBloomType(hcol.getBloomFilterType())
+        .build();
 
     return new RecordWriter<Object, Cell>() {
       @Override
