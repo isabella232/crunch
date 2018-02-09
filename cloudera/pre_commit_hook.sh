39d979369c74bd37ebd6817f606bfda3c6422266
Carl Myers <cmyers@cloudera.com>
2018-02-09 11:21:26 -0800

    RELENG-3084 - modify precommit jobs to use new unittest infra
    This change prevents the need to put scripts in the working tree,
    leaving git clean during the build.
    
    Change-Id: Iac9bc02ee37dbd68c841c81d39a788bdf770c583

--- cloudera/pre_commit_hook.sh
+++ cloudera/pre_commit_hook.sh
@@ -6,4 +6,7 @@ export JAVA8_BUILD=true
 . /opt/toolchain/toolchain.sh
 export PATH="$MAVEN_3_5_0_HOME/bin:$PATH"
 
+# activate mvn-gbn wrapper
+mv "$(which mvn-gbn-wrapper)" "$(dirname "$(which mvn-gbn-wrapper)")/mvn"
+
 mvn -Drat.numUnapprovedLicenses=10000 clean test package
